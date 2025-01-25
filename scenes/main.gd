2f5c6f9af73476a68559a7d465808695620487d3
indie Hagov Dev <nikolas.martino@gmail.com>
2025-01-25 18:57:13 -0300

    4to commit

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,27 +1,43 @@
 extends Node2D
-@onready var vaso: TextureProgressBar = %Vaso
+@onready var vaso: TextureProgressBar = %RellenoCoca
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var perdiste: Label = %Perdiste
-@onready var cantidad: Label = %Cantidad
+@onready var cantidad_fernet: Label = %CantidadFrNeTh
+@onready var cantidad_coca: Label = %CantidadCoCa
 @onready var restart: Button = $Restart
 @onready var explicacion: Label = $Explicacion
+@onready var check_fr_ne_th: Sprite2D = $FrNeTh/CheckFrNeTh
+@onready var check_co_ca: Sprite2D = $FrNeTh/CheckCoCa
+@onready var relleno_fernet: TextureProgressBar = $RellenoFernet
 
 const max_coca:float = 70
 const max_ferne:float = 30
 
 
 var _progress :float = 0.
+var _progress_fernet :float = 70.
 var animacion_finalizada :bool = false
+#true es coca false es ferne
+var bebida : bool = true
 
 func _ready() -> void:
 	animation_player.animation_finished.connect(animacion)
+	cantidad_fernet.text = str(0)+"%"
+	cantidad_coca.text = str(0)+"%"
+	relleno_fernet.value = 70.
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("fill") and animacion_finalizada:
+	if Input.is_action_pressed("fill") and animacion_finalizada and bebida:
 		_progress += randf_range(0.1,0.5)
-		print(vaso.value)
-	vaso.value = _progress
-	cantidad.text = str(vaso.value)+"%"
+		print(_progress)
+		vaso.value = _progress
+	elif Input.is_action_pressed("fill") and animacion_finalizada and not bebida:
+		_progress_fernet += randf_range(0.1,0.5)
+		print(_progress_fernet)
+		relleno_fernet.value = _progress_fernet
+	
+	cantidad_coca.text = str(vaso.value)+"%"
+	cantidad_fernet.text = str(relleno_fernet.value-70.)+"%"
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("fill"):
@@ -36,8 +52,17 @@ func _input(event: InputEvent) -> void:
 			perdiste.show()
 			restart.show()
 		if vaso.value == max_coca:
-			perdiste.text = "GANASTE"
+			#perdiste.text = "GANASTE"
+			#perdiste.show()
+			check_co_ca.show()
+			bebida = false
+			relleno_fernet.show()
+		if relleno_fernet.value > 100:
+			perdiste.text = "PERDISTE"
 			perdiste.show()
+			restart.show()
+		if relleno_fernet.value == 100:
+			check_fr_ne_th.show()
 			restart.show()
 
 func animacion(animacion: String) -> void:
